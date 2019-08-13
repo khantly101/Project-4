31771c4991344f46ba7cfe3ca115d7b67a7515a3
khanly101 <khantuanly@gmail.com>
2019-08-13 00:30:46 -0700

    Updates

--- precache-manifest.b845d033cdca35f20ef7ef1a95be4f81.js
+++ precache-manifest.b845d033cdca35f20ef7ef1a95be4f81.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6cb3d80bbb27bfff8a9456bf22561944",
+    "revision": "817be4fd522ff6ecfafe351c58a8220f",
     "url": "/sudoku-front-end/index.html"
   },
   {
-    "revision": "31b7e522c380bfb33427",
+    "revision": "117e8de3f4e1e735c244",
     "url": "/sudoku-front-end/static/css/2.1f156bdb.chunk.css"
   },
   {
-    "revision": "b57c944c9325f8c9219e",
+    "revision": "38059d6716b7e51ea923",
     "url": "/sudoku-front-end/static/css/main.7e51a94a.chunk.css"
   },
   {
-    "revision": "31b7e522c380bfb33427",
-    "url": "/sudoku-front-end/static/js/2.4fe9f948.chunk.js"
+    "revision": "117e8de3f4e1e735c244",
+    "url": "/sudoku-front-end/static/js/2.74c55208.chunk.js"
   },
   {
-    "revision": "b57c944c9325f8c9219e",
-    "url": "/sudoku-front-end/static/js/main.dda89983.chunk.js"
+    "revision": "38059d6716b7e51ea923",
+    "url": "/sudoku-front-end/static/js/main.9e1d4aac.chunk.js"
   },
   {
     "revision": "eb8570d0b4a5a822c132",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sudoku-front-end/precache-manifest.0e7ec4751571df768879763a6fc2c13a.js"
+  "/sudoku-front-end/precache-manifest.b845d033cdca35f20ef7ef1a95be4f81.js"
 );
 
 self.addEventListener('message', (event) => {
