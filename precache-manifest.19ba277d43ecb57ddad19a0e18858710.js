066c1ac3d16641c549ab8ed156f96b8ade210752
khanly101 <khantuanly@gmail.com>
2019-08-12 23:47:39 -0700

    Updates

---
+++ precache-manifest.19ba277d43ecb57ddad19a0e18858710.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "934523db390964284bb5fbef97ecdfff",
+    "url": "/sudoku-front-end/index.html"
+  },
+  {
+    "revision": "31b7e522c380bfb33427",
+    "url": "/sudoku-front-end/static/css/2.1f156bdb.chunk.css"
+  },
+  {
+    "revision": "64ca9f4c51156ed17eb0",
+    "url": "/sudoku-front-end/static/css/main.b1fc9755.chunk.css"
+  },
+  {
+    "revision": "31b7e522c380bfb33427",
+    "url": "/sudoku-front-end/static/js/2.4fe9f948.chunk.js"
+  },
+  {
+    "revision": "64ca9f4c51156ed17eb0",
+    "url": "/sudoku-front-end/static/js/main.b7ca3dc2.chunk.js"
+  },
+  {
+    "revision": "eb8570d0b4a5a822c132",
+    "url": "/sudoku-front-end/static/js/runtime~main.cc10579b.js"
+  }
+]);
